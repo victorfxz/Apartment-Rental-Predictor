2ee5a212adc2662fa705b457764e586fccf64ece
Victor Felipe de Lima <123201667+victorfxz@users.noreply.github.com>
2024-07-22 18:05:09 -0300

    Add files via upload

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -106,7 +106,7 @@ return (
         Estimate Price
       </button>
       {isLoading && <div style={{ textAlign: 'center', fontSize: '20px', marginTop: '20px', color: 'white' }}>Loading...</div>}
-      <div style={{ textAlign: 'center', fontSize: '20px', marginTop: '20px', color: 'white' }}>{prediction}</div>
+      <div className={(prediction.includes('Please enter all valid values to obtain a prediction!') || prediction.includes('Error')) ? 'error-message' : ''} style={{ textAlign: 'center', fontSize: '20px', marginTop: '20px', color: 'white' }}>{prediction}</div>
     </div>
   </div>
 );
