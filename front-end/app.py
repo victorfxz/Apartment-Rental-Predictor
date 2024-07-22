66907765d4dbef26c7a61d1dc5bd15cdb79efad5
Victor Felipe de Lima <123201667+victorfxz@users.noreply.github.com>
2024-07-22 19:32:02 -0300

    Update app.py

--- front-end/app.py
+++ front-end/app.py
@@ -33,7 +33,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Treina o modelo
 model = train_model(X_train, y_train)
 
-@app.route('/front-end/predict', methods=['GET'])
+@app.route('/front-end/app.py', methods=['GET'])
 def home():
     return "API de Previsão de Preços Imobiliários"
 
