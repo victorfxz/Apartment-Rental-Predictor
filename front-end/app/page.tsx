cbbd820faab6881b3f9594e8947508f62f7c6af9
Victor Felipe de Lima <123201667+victorfxz@users.noreply.github.com>
2024-07-22 19:38:53 -0300

    Update page.tsx

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -36,7 +36,7 @@ function Home() {
           'size': Number(areaConstruida),
         };
 
-		const response = await axios.post('http://localhost:5000/predict', data);
+		const response = await axios.post('/predict', data);
 		if (response.data.predicted_price !== undefined) {
 		  const predictedPrice = response.data.predicted_price / 100;
 		  const formattedPrice = predictedPrice.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
